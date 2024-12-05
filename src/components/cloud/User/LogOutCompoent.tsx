1656776357314bfa5a947bc7ec73158b4c06cc17
dmitrykhay <113887425+dmitrykhay@users.noreply.github.com>
2024-12-05 17:53:42 +0300

    Update LogOutCompoent.tsx

--- src/components/cloud/User/LogOutCompoent.tsx
+++ src/components/cloud/User/LogOutCompoent.tsx
@@ -14,7 +14,7 @@ export function LogOutComponent(){
     const handleLogOut = async () =>{
         if(user && token){
             try{
-                const baseUrl = 'http://89.104.69.194:8000'
+                const baseUrl = 'http://79.174.86.223:8000'
                 const url = `${baseUrl}/cloud/log_out/${user.id}` 
                 console.log(url);
                 const response = await axios.get(url,{
