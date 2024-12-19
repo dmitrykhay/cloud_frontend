aa1fcd6178d6f5052665d12436b7bd14d3c17e35
dmitrykhay <113887425+dmitrykhay@users.noreply.github.com>
2024-12-19 11:56:52 +0300

    Update ADownloadById.tsx

--- src/components/Admin/File/ADownloadById.tsx
+++ src/components/Admin/File/ADownloadById.tsx
@@ -14,7 +14,7 @@ export function ADownloadById({ userId, fileId, name}: Props) {
   const handleGetFile = async () => {
     try {
       if(token){
-        const basUrl = 'http://79.174.86.223:8000';
+        const basUrl = 'http://79.174.94.152:8000';
         const url = `${basUrl}/cloud/admin/get_file/${userId}/${fileId}`
         const response = await axios.get(
           url,
